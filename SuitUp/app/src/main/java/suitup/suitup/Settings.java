0d5f392aa42c1dfef7484d1736470fd95ecce2a8
Dalia Maarek <daliamaarek@Dalias-MacBook-Pro-2.local>
2015-11-30 23:05:13 +0200

    Settingssss

--- SuitUp/app/src/main/java/suitup/suitup/Settings.java
+++ SuitUp/app/src/main/java/suitup/suitup/Settings.java
@@ -1,6 +1,7 @@
 package suitup.suitup;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -19,15 +20,21 @@ public class Settings extends Activity {
         final SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         editor = settings.edit();
         fname = (EditText) findViewById(R.id.fNameText);
-        fname.setText(settings.getString());
+        fname.setText(settings.getString("fname", "First Name"));
         lname = (EditText) findViewById(R.id.lNameText);
-        lname.setText(settings.getString());
+        lname.setText(settings.getString("lname", "Last Name"));
         email = (EditText) findViewById(R.id.emailText);
-        email.setText(settings.getString());
+        email.setText(settings.getString("email", "Email"));
     }
 
     public void saveChanges(View v){
-        editor.putString().putString().putString().commit();
+        editor.putString("fname",fname.getText().toString()).putString("lname", lname.getText().toString()).putString("email", email.getText().toString()).commit();
+        Intent intent = new Intent(getApplicationContext(), UserProfileActivity.class);
+        startActivity(intent);
+    }
 
+    public void LogOutClicked(View v){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
