89cd83ac799984250a27f7c61d0e89ffceac6a9d
SarahDesouky <sarahdesouky94@gmail.com>
2015-12-01 00:03:40 +0200

    Unread Messages now have different color (Blue)

--- SuitUp/app/src/main/java/suitup/suitup/AllMessagesActivity.java
+++ SuitUp/app/src/main/java/suitup/suitup/AllMessagesActivity.java
@@ -1,13 +1,16 @@
 package suitup.suitup;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.AdapterView;
 import android.view.View;
 import android.content.Intent;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -29,8 +32,22 @@ public class AllMessagesActivity extends Activity {
         msgs.add(new Message("Message 4", "Doha", "Alaa"));
         msgs.add(new Message("Message 5", "Lina", "Alaa"));
 
-        ArrayAdapter<Message> adapter = new ArrayAdapter<Message>(this, android.R.layout.simple_list_item_1, msgs);
-        listView.setAdapter(adapter);
+        ArrayAdapter<Message> adapter = new ArrayAdapter<Message>(this,
+                android.R.layout.simple_list_item_1, msgs) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text = (TextView) view.findViewById(android.R.id.text1);
+                if(position%2==0)
+                text.setTextColor(Color.BLUE);
+                else
+                text.setTextColor(Color.BLACK);
+                return view;
+            }
+        };
+
+//        ArrayAdapter<Message> adapter = new ArrayAdapter<Message>(this, android.R.layout.simple_list_item_1, msgs);
+       listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new OnItemClickListener() {
             @Override
@@ -39,6 +56,7 @@ public class AllMessagesActivity extends Activity {
 //                Toast.makeText(getBaseContext(), item, Toast.LENGTH_LONG).show();
 //                String e = "" + id;
 //                Toast.makeText(getBaseContext(), e, Toast.LENGTH_LONG).show();
+                ((TextView)view.findViewById(android.R.id.text1)).setTextColor(Color.BLACK);
                 msg = msgs.get((int)id);
                 Intent intent = new Intent(AllMessagesActivity.this, MessageActivity.class);
                 AllMessagesActivity.this.startActivity(intent);
