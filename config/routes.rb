c894e6b3936beb8128b5ede7ffeaabf1f81e691f
Dalia Maarek <daliamaarek@Dalias-MacBook-Pro-3.local>
2015-12-15 18:23:57 +0200

    new method in rails

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   post '/users/:twitter_id/friends' => 'users#addFriend'
   delete '/users/:twitter_id/friends/:id' => 'users#removeFriend'
 
+  get '/users/:id/posts' => 'posts#getMyPostsByID'
 
   get '/users/:twitter_id/posts' => 'posts#getMyPosts'
 
