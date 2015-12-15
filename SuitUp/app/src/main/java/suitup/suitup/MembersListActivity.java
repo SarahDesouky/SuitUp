a464b9e490fdaf1aa73cacb8b6f1d1ef903ab97b
Dalia Maarek <daliamaarek@Dalias-MacBook-Pro-3.local>
2015-12-15 13:24:28 +0200

    7ozn

--- SuitUp/app/src/main/java/suitup/suitup/MembersListActivity.java
+++ SuitUp/app/src/main/java/suitup/suitup/MembersListActivity.java
@@ -1,10 +1,13 @@
 package suitup.suitup;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,32 +20,31 @@ import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-public class MembersListActivity extends ListActivity {
+public class MembersListActivity extends AppCompatActivity{
 
-    UserListAdapter adapter1;
+    FriendsAdapter adapter1;
+    ListView usersList;
+    public static SharedPreferences.Editor editor;
+    public static final String PREFS_NAME = "MyPrefs";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_members_list);
-//        for(int i=0; i<StaticData.members.length; i++){
-//            content.add(StaticData.members[i].getUsername());
-//        }
-//        for(int i =0;i<content.size();i++)
-//            icons.add(R.drawable.mohee);
         RestAdapter adapter = new RestAdapter.Builder().setEndpoint(getResources().getString(R.string.API_BASE_URL)).build();
         ourAPI api = adapter.create(ourAPI.class);
+
+        final SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        editor = settings.edit();
 
         api.getAllUsers(new Callback<List<models.User>>() {
             @Override
             public void success(List<User> users, Response response) {
                 Log.d("suzzess", "suzzess");
-                ArrayList<User> u = new ArrayList<>();
-                for(int i =0;i<users.size();i++){
-                    u.add(users.get(i));
-                    Log.d(users.get(i).getFname(), users.get(i).getFname());
-                }
-                adapter1 = new UserListAdapter(MembersListActivity.this, u);
+                Log.d("User1",users.get(0).getAvatar_url());
+                Log.d("User2",users.get(1).getAvatar_url());
+                adapter1 = new FriendsAdapter(MembersListActivity.this, users);
             }
 
             @Override
@@ -50,14 +52,17 @@ public class MembersListActivity extends ListActivity {
                 Log.d("nooooo","nooo");
             }
         });
-        setListAdapter(adapter1);
-    }
+        usersList = (ListView) findViewById(R.id.usersList);
+        usersList.setAdapter(adapter1);
+        usersList.setOnItemClickListener(new android.widget.AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                models.User user = (models.User) parent.getItemAtPosition(position);
+                editor.putString("user_id", String.valueOf(user.getId())).commit();
+                Intent intent = new Intent(getApplicationContext(), MemberProfile.class);
+                startActivity(intent);
+            }
+        });
 
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        Intent intent = new Intent(getApplicationContext(), MemberProfile.class);
-//        intent.putExtra("friendName", content.get(position));
-        startActivity(intent);
     }
 
 }
